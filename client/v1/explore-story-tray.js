ea3f8a11bb8573c1a2e152778662c1e3c2edd51a
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2016-12-18 01:14:31 -0800

    Fix ExploreStoriesTray object property

--- client/v1/explore-story-tray.js
+++ client/v1/explore-story-tray.js
@@ -14,6 +14,6 @@ ExploreStoryTray.prototype.get = function () {
   .setResource('exploreStoryTray')
   .send()
   .then(function(data) {
-    return data.items[0].stories.tray;
+    return data.items[0].stories;
   });
 };
